7482a8d9e552bcc66b3ccf88c875c9aca3c8e2e3
Balacke <starnerd21@gmail.com>
2026-05-16 17:29:28 -0600

    Cool pics responsive menu and modal

---
+++ prove/javascript/cool_pics.js
@@ -0,0 +1,44 @@
+// Menu
+let menuBtn = document.querySelector(".small");
+let nav = document.querySelector("nav");
+
+menuBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    nav.classList.toggle("visible");
+});
+
+
+    
+
+
+
+// Modal Image
+
+const gallery = document.querySelector('.images');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+
+    // Code to show modal  - Use event parameter 'e'   
+    console.log(e.target.src);
+    console.log(e.target.src.replace("sm", "full"));
+    modalImage.src = e.target.src.replace("sm", "full");
+    modal.showModal();
+
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
